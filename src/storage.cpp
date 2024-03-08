c8b795fbebedf9c0fdcb0ce7fefae5d551b54c83
ImedElh <imhobsystems@gmail.com>
2024-03-08 11:16:41 +0100

    add storage module

---
+++ src/storage.cpp
@@ -0,0 +1,83 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-save-data-permanently-preferences/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <Preferences.h>
+
+Preferences preferences;
+
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  //Create a new storage space on the flash memory with the credentials namespace.
+  // false= able to read and write
+  preferences.begin("credentials", false);
+  // Create 2 keys by using the putString() method.
+  preferences.putString("ssid", ssid); 
+  preferences.putString("password", password);
+
+  Serial.println("Network Credentials Saved using Preferences");
+  // close the preferences 
+  preferences.end();
+}
+
+#ifdef GET_CRED
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-save-data-permanently-preferences/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <Preferences.h>
+#include "WiFi.h"
+
+Preferences preferences;
+
+String ssid;
+String password;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  
+  preferences.begin("credentials", false);
+ 
+  ssid = preferences.getString("ssid", ""); 
+  password = preferences.getString("password", "");
+
+  if (ssid == "" || password == ""){
+    Serial.println("No values saved for ssid or password");
+  }
+  else {
+    // Connect to Wi-Fi
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(), password.c_str());
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print('.');
+      delay(1000);
+    }
+    Serial.println(WiFi.localIP());  
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+#endif
